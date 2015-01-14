6031f818c0d5e143a264a994da6daf11019fc904
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-01-14 15:27:41 -0600

    print hello world

---
+++ src/todo.js
@@ -0,0 +1,15 @@
+(function () {
+  
+  window.TodoApp = {}
+  
+  TodoApp.controller = function (){
+    var ctrl = {}
+    return ctrl;
+  }
+  
+  TodoApp.view = function (ctrl) {
+      return m('.todos', "Hello World")
+    }
+    
+  
+})()
